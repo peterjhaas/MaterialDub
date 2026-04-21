788a8a751865f6b1f99ed6847f25f4e14bb67aaf
Peter J. Haas <herrhaas@gmail.com>
2026-04-21 00:37:01 -0400

    Initial commit
    this is the splash screen start up, adds a nice graphic, preps the user for use.  If the user wants to skip this they can run the other shell file.
    
    Hopefully will become the collection screen for allowing the user to configure the config file.

---
+++ MediaDub.sh
@@ -0,0 +1,23 @@
+clear 
+
+
+
+
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$0")
+# Access a file in that directory
+THISVERSION="1.0.1"
+                                         
+echo "\n"
+echo "▗▖  ▗▖▗▄▄▄▖▗▄▄▄ ▗▄▄▄▖ ▗▄▖     ▗▄▄▄ ▗▖ ▗▖▗▄▄▖ "
+echo "▐▛▚▞▜▌▐▌   ▐▌  █  █  ▐▌ ▐▌    ▐▌  █▐▌ ▐▌▐▌ ▐▌"
+echo "▐▌  ▐▌▐▛▀▀▘▐▌  █  █  ▐▛▀▜▌    ▐▌  █▐▌ ▐▌▐▛▀▚▖"
+echo "▐▌  ▐▌▐▙▄▄▖▐▙▄▄▀▗▄█▄▖▐▌ ▐▌    ▐▙▄▄▀▝▚▄▞▘▐▙▄▞▘"
+echo  "\nV."$THISVERSION
+echo "\nCreated by Peter J. Haas\n"                                           
+                                            
+                                   
+
+read -p "Press [Enter] key to continue..."   
+
+source $SCRIPT_DIR/FeatureSync.sh
